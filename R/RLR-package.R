2c5368d6794b2ef68f0b96c7b6c8bb17a14db217
idoba2030 <ido.iba@gmail.com>
2021-12-13 16:10:08 +0200

    documentation

--- R/RLR-package.R
+++ R/RLR-package.R
@@ -1,4 +1,8 @@
-#' @keywords internal
+#' RLR - All you Need for Your Reinforcement Learning Experiment
+#'
+#' @description This package provides helper function necessary for RL research.
+#' @seealso \href{https://www.shahar-lab.com/}{Computational Clinical Psychology Lab}
+#' @author Ido Ben Artzi, \email{ido.iba@gmail.com} ; Nitzan Shahar, \email{shahar.nitzan@gmail.com}
 "_PACKAGE"
 
 ## usethis namespace: start
