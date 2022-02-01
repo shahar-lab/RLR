fe89a936c886957f0937f85354e49457d58f2414
idoba2030 <ido.iba@gmail.com>
2022-02-01 16:29:36 +0200

    my_posteriorplot

--- R/my_posteriorplot.R
+++ R/my_posteriorplot.R
@@ -4,7 +4,7 @@
 #' @param myxlabel x label
 #' @param mycolor color of density function
 #' @param myxlim a vector of two numbers describing the limits of the x axis.
-#' @param myvline the value of the median or the mean.
+#' @param my_vline the value of the median or the mean.
 #' @return Returns a posterior plot
 #' @details This function plots a posterior from your bayesian outputs.
 #' @examples
@@ -14,7 +14,7 @@
 #'myxlabel="Beta1",
 #'mycolor = "gray",
 #'myxlim = c(0,2),
-#'myvline= median(param)
+#'my_vline= median(param)
 #')
 #'}
 #' @seealso
@@ -23,12 +23,9 @@
 #' @import ggplot2
 
 my_posteriorplot<-function(x,myxlabel,mycolor,myxlim,my_vline){
-
   library(ggplot2)
-
   ggplot(data.frame(x=x),aes(x=x))+geom_density(alpha = .5,fill=mycolor)+
     geom_vline(xintercept = my_vline, linetype="dotted",color = "blue", size=1.5)+
-    geom_segment(aes(x = hdi(x, ci = 0.95)$CI_low, y = 0, xend = x, yend = 0),color="darkgray",size=2,show.legend = F)+
+    geom_segment(aes(x = hdi(x, ci = 0.95)$CI_low, y = 0, xend = hdi(x, ci = 0.95)$CI_high, yend = 0),color="darkgray",size=2,show.legend = F)+
     xlim(myxlim[1],myxlim[2])+ xlab(myxlabel)+ theme_classic()
-
 }
